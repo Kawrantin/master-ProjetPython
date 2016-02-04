988389f49525ad6724bf2ca1f78469ae1b58239f
Marina MONTFORT <E145457F@U-INF-J-F113-13.iut-nantes.univ-nantes.prive>
2016-02-04 10:23:48 +0100

    import

---
+++ import.py
@@ -0,0 +1,26 @@
+import csv
+
+fname = "data/activites.csv"
+file = open(fname, "r")
+
+fname2 = "data/equipements.csv"
+file2 = open(fname2, "r")
+
+fname3 = "data/installations.csv"
+file3 = open(fname3, "r")
+ 
+try:
+    reader = csv.reader(file)
+    for row in reader:
+        print (row)
+    reader2 = csv.reader(file2)
+    for row in reader2:
+        print (row)
+        
+    reader3 = csv.reader(file3)
+    for row in reader3:
+        print (row)
+finally:
+    file.close()
+    file2.close()
+    file3.close()
