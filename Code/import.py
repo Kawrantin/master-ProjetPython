967f25675db588c017e6050ced3d6a0bffc03613
Kawrantin <tpe.prothese.jeanperrin@gmail.com>
2016-02-25 09:52:50 +0100

    Test import.py

--- Code/import.py
+++ Code/import.py
@@ -1,6 +1,6 @@
 import csv
 
-fname = "data/activites.csv"
+fname = "data/activite.csv"
 file1 = open(fname, "r")
 
 fname2 = "data/equipements.csv"
@@ -10,17 +10,45 @@ fname3 = "data/installations.csv"
 file3 = open(fname3, "r")
  
 try:
-    reader = csv.reader(file1, delimiter=',')
+    reader = csv.reader(file1, delimiter=',', quotechar='"')
     for row in reader:
         print (row)
-    reader2 = csv.reader(file2)
+    reader2 = csv.reader(file2, delimiter=',', quotechar='"')
     for row in reader2:
         print (row)
         
-    reader3 = csv.reader(file3)
+    reader3 = csv.reader(file3, delimiter=',', quotechar='"')
     for row in reader3:
         print (row)
 finally:
     file1.close()
     file2.close()
     file3.close()
+
+
+
+# import csv
+# fname = "data/activite.csv"
+# # file1 = open(fname, "r")
+
+# # fname2 = "data/equipements.csv"
+# # file2 = open(fname2, "r")
+
+# # fname3 = "data/installations.csv"
+# # file3 = open(fname3, "r")
+ 
+# with open(fname, 'rt') as f:
+
+#     reader = csv.reader(f, delimiter=',', quotechar='"')
+#     for row in reader:
+#         act = Activite(row[1], row[0])
+#         print(act)
+
+# f.close();
+#     # reader2 = csv.reader(file2, delimiter=',', quotechar='"')
+#     # for row in reader2:
+#     #     print (row)
+        
+#     # reader3 = csv.reader(file3, delimiter=',', quotechar='"')
+#     # for row in reader3:
+#     #     print (row)
