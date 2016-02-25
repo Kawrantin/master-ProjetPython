7f591f44d006a04a10c1e3b25336d93d0623379e
Kanaii <Claire.laurencot@Laposte.net>
2016-02-25 09:47:54 +0100

    Modification de la classe de gestion de la BDD

--- Code/GestionEquipement.py
+++ Code/GestionEquipement.py
@@ -19,12 +19,15 @@ class GestionBD(object):
 					id varchar(50) DEFAULT NULL,
 				    nom varchar(50) DEFAULT NULL,
 					id_installation INTEGER DEFAULT NULL,
-				    PRIMARY KEY(id))""")
+				    PRIMARY KEY(id),
+					FOREIGN KEY (id_installation) REFERENCES installation(id) ON DELETE CASCADE ON UPDATE CASCADE)
+				DEFAULT CHARSET=utf8 """)
 			cursor.execute("""
 				CREATE TABLE IF NOT EXISTS activite (
 					idAct int(5) NOT NULL AUTO_INCREMENT,
 					nom varchar(50) DEFAULT NULL,
-				    PRIMARY KEY(idAct)) """)
+				    PRIMARY KEY(idAct))
+				DEFAULT CHARSET=utf8  """)
 			cursor.execute("""
 				CREATE TABLE IF NOT EXISTS installation (
 				    id varchar(50) DEFAULT NULL,
@@ -34,12 +37,21 @@ class GestionBD(object):
 				    ville varchar(50) DEFAULT NULL,
 				    latitude INTEGER DEFAULT NULL,
 				    longitude INTEGER DEFAULT NULL,
-				    PRIMARY KEY(id)) """)
+				    PRIMARY KEY(id))
+				DEFAULT CHARSET=utf8  """)
+			cursor.execute("""
+				CREATE TABLE IF NOT EXISTS Equip_Activ (
+				    id_equip varchar(50) DEFAULT NULL,
+				    id_activ varchar(50) DEFAULT NULL,
+					FOREIGN KEY (id_equip) REFERENCES equipement(id) ON DELETE CASCADE ON UPDATE CASCADE,
+					FOREIGN KEY (id_activ) REFERENCES activite(idAct) ON DELETE CASCADE ON UPDATE CASCADE
+				DEFAULT CHARSET=utf8  """)
 		except:
 			print("ERREUR : La table existe déjà.")
-		self.conn.commit()
+		else:
+			self.conn.commit()
 
-
+"""Fonctions d'ajouts"""
 	def insertInstall(self, ide, nom, adresse, codePostal, ville, latitude, longitude):
 		cursor = self.conn.cursor()
 		try:
@@ -47,93 +59,69 @@ class GestionBD(object):
 				INSERT INTO pagesTEST (id, nom, adresse, codePostal, ville, latitude, longitude)
 				VALUES(%s, %s, %s, %s, %s, %s, %s)""", (ide, nom, adresse, codePostal, ville, latitude, longitude))
 		except:
-			print("La page existe déjà.")
+			print("L'installation existe déjà.")
+		else:
 			self.conn.commit()
 
-	def insertActivite(self, nom):
+
+	def insertActivite(self, ide, nom):
 		cursor = self.conn.cursor()
 		try:
 			cursor.execute("""
-				INSERT INTO pagesTEST (numInstall, nomInstall, numAdresse, adresse, codePostal, ville, latitude, longitude)
-				VALUES(%s, %s, %s, %s, %s, %s, %s, %s)""", (numInstall, nomInstall, numAdresse, adresse, codePostal, ville, latitude, longitude))
+				INSERT INTO pagesTEST (id, nom)
+				VALUES(%s, %s)""", (ide, nom))
 		except:
-			print("La page existe déjà.")
+			print("L'activité existe déjà.")
+		else:
 			self.conn.commit()
 
 
-"""Getters:"""
-	def getAdresse(self, ide):
+	def insertEquip(self, ide, nom, id_installation):
 		cursor = self.conn.cursor()
 		try:
-			cursor.execute("""SELECT adPage FROM pagesTEST WHERE idPage = %s""", [ide])
+			cursor.execute("""
+				INSERT INTO pagesTEST (id, nom, id_installation)
+				VALUES(%s, %s, %s)""", (ide, nom, id_installation))
 		except:
-			print("ERREUR : Indentifiant inexistant.")
+			print("L'equipement existe déjà.")
 		else:
-		row = cursor.fetchone()
-		while row :
-			# print(row)
-			if row is not None:
-				break
-			row = cursor.fetchone()
-		return row[0]
+			self.conn.commit()
 
 
-	def getNbLien(self, ide): #Fonctionne
+	def insertEquip(self, id_equip, id_activ):
 		cursor = self.conn.cursor()
 		try:
-		cursor.execute("""SELECT nbLiens FROM pagesTEST WHERE idPage = %s""", [ide]) # %s qui bug
+			cursor.execute("""
+				INSERT INTO pagesTEST (id_equip, id_activ)
+				VALUES(%s, %s)""", (id_equip, id_activ))
 		except:
-			print("ERREUR : Indentifiant inexistant.")
+			print("L'ensemble existe déjà.")
 		else:
-		row = cursor.fetchone()
-		while row :
-			if row is not None:
-				break
-			row = cursor.fetchone()
-		return row[0]
-
-	def getPageRank(self, ide):
-		cursor = self.conn.cursor()
-		cursor.execute("""SELECT pRank FROM pagesTEST WHERE idPage = %s""", [ide])
-		except:
-			print("ERREUR : Indentifiant inexistant.")
-		else:
-		row = cursor.fetchone()
-		while row :
-			if row is not None:
-				break
-			row = cursor.fetchone()
-		return row[0]
-
-	def getId(self, addr):
-		cursor = self.conn.cursor()
-		cursor.execute("""SELECT idPage FROM pagesTEST WHERE adPage = %s """, [addr])
-		except:
-			print("ERREUR : Indentifiant inexistant.")
-		else:
-		row = cursor.fetchone()
-		while row :
-			if row is not None:
-				break
-			row = cursor.fetchone()
-		return row[0]
+			self.conn.commit()
 
 
-"""Setters:"""
-	def setAdresse(self, ide, adPage):
-		cursor = self.conn.cursor()
-		cursor.execute("""UPDATE pagesTEST SET adPage = %s WHERE idPage = %s """, (adPage, ide))
-		self.conn.commit()
+"""Getters: (Juste la forme)"""
+	# def getAdresse(self, ide):
+	# 	cursor = self.conn.cursor()
+	# 	try:
+	# 		cursor.execute("""SELECT adPage FROM pagesTEST WHERE idPage = %s""", [ide])
+	# 	except:
+	# 		print("ERREUR : Indentifiant inexistant.")
+	# 	else:
+	# 	row = cursor.fetchone()
+	# 	while row :
+	# 		# print(row)
+	# 		if row is not None:
+	# 			break
+	# 		row = cursor.fetchone()
+	# 	return row[0]
 
-	def setNbLien(self, ide, nbLiens):
-		cursor = self.conn.cursor()
-		cursor.execute("""UPDATE pagesTEST SET nbLiens = %s WHERE idPage = %s """, (nbLiens, ide))
-		self.conn.commit()
+"""Setters: (Juste la forme)"""
+	# def setAdresse(self, ide, adPage):
+	# 	cursor = self.conn.cursor()
+	# 	cursor.execute("""UPDATE pagesTEST SET adPage = %s WHERE idPage = %s """, (adPage, ide))
+	# 	self.conn.commit()
 
-	def setPageRank(self, ide, pRank):
-		cursor = self.conn.cursor()
-		cursor.execute("""UPDATE pagesTEST SET pRank = %s WHERE idPage = %s """, (pRank, ide))
-		self.conn.commit()
-
+"""Fonction de déconnexion de la BDD"""
 	def deco(self):
 		self.conn.close()
