c6775981ca4012405060119b11c52107159ef434
Kawrantin <tpe.prothese.jeanperrin@gmail.com>
2016-03-17 08:48:19 +0100

    Re-organisation du projet

--- Code/src/rest_serv.py
+++ Code/src/rest_serv.py
@@ -1,29 +1,7 @@
 from libs.bottle import route, template, run
 
-@route('/hello/<name>/<nom>')
-def index(name, nom):
-    return template('<b>Hello {{name}}</b>! <br> <b>Hello {{nom}}</b>! ', name=name, nom=nom)
+@route('/hello/<name>/')
+def index(name):
+    return template('<b>Hello {{name}}</b>!', name=name)
 
-run(host='infoweb', port=8080)
-
-import _mysql
-import sys
-
-try:
-    con = _mysql.connect('infoweb', 'E145425W', 'E145425W', 'E145425W')
-        
-    con.query("SELECT VERSION()")
-    result = con.use_result()
-    
-    print "MySQL version: %s" % \
-        result.fetch_row()[0]
-    
-except _mysql.Error, e:
-  
-    print "Error %d: %s" % (e.args[0], e.args[1])
-    sys.exit(1)
-
-finally:
-    
-    if con:
-        con.close()
+run(host='localhost', port=8080)
