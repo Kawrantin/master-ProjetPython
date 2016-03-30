6e7fd5babae8f52c11d99b56d7e8200543d1b703
Marina MONTFORT <E145457F@U-INF-J-F222-02.iut-nantes.univ-nantes.prive>
2016-03-30 12:44:10 +0200

    modif rest

--- Code/src/rest_serv.py
+++ Code/src/rest_serv.py
@@ -10,28 +10,31 @@ import urllib
 
 
 
-@route('/api/<item>')
+@route('/api/<item>') #contenu de l'url
 def show_id(item):
     retour = ""
     ret = []
-    conn = mysql.connector.connect(host="infoweb", user="E145425W", password="E145425W", database="E145425W")
+    conn = mysql.connector.connect(host="infoweb", user="E145425W", password="E145425W", database="E145425W") #connexion a la base de donnees
     c = conn.cursor()
-    c.execute("SELECT e.nom FROM equipement e JOIN equip_activ ea ON e.id = ea.id_equip JOIN activite a ON ea.id_activ = a.idAct WHERE a.nom LIKE '%"+[item]+"%'") 
+    c.execute("SELECT e.nom FROM equipement e JOIN equip_activ ea ON e.id = ea.id_equip JOIN activite a ON ea.id_activ = a.idAct WHERE a.nom LIKE'%"+item+"%'")
+    #on cherche le nom des equipements pour l'activite recherchee
     result = c.fetchall()
-
+    #cela nous renvoie un tableau des differentes reponses 
     for row in result:
     	if row[0] not in ret:
-    		retour = retour + str(row[0]) + ", "
+    		retour = retour + str(row[0]) + " | \n"
+            #on ajoute a la reponse chaque valeur du tableau
     		ret.append(row[0])
     c.close()
     if not result:
+        #si la valeur de la recherche n'est pas presente dans la base de donnees
         return 'This item number does not exist!'
     else:
         return template('Résultat :  <br> {{itemr}}', itemr=retour)
 
 @route('/<filepath:path>')
 def server_static(filepath):
-    return static_file(filepath, root='./view')
+    return static_file(filepath, root='./view') 
 
 run(host='localhost', port=8080)
 
